e4c4bb46bd884a3c0b9588cc225c2a7cc4faf2b9
GilsonGarciaJunior <juniorrodrigues082006@gmail.com>
2026-07-24 19:12:09 -0300

    Adicionando Admin

--- Meu_site/blog/admin.py
+++ Meu_site/blog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("titulo", "conteudo")
+
+admin.site.register(Post, PostAdmin)
